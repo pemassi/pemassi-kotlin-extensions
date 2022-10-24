f3aeb612b10cec9ac0f221e70b773762d3a9d8d7
Kyungyoon Kim <ruddbs5302@gmail.com>
2022-10-24 23:39:52 +0900

    feat: catchOrDefault extension

--- src/main/kotlin/io/pemassi/kotlin/extensions/grammer/TryExtension.kt
+++ src/main/kotlin/io/pemassi/kotlin/extensions/grammer/TryExtension.kt
@@ -19,3 +19,20 @@ fun tryOrIgnore(printException: Boolean = true, f: () -> Unit) {
             e.printStackTrace()
     }
 }
+
+inline fun <T, reified E: Throwable> catchOrDefault(default: T, f: () -> T): T {
+    return try {
+        f()
+    }
+    catch (e: Throwable)
+    {
+        when(e) {
+            is E -> default
+            else -> throw e
+        }
+    }
+}
+
+inline fun <T, reified E: Throwable> catchOrNull(f: () -> T): T? {
+    return catchOrDefault<T?, E>(null, f)
+}
