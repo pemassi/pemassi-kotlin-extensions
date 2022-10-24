603b3d7398c03bcbe54d5ee566b48d6d51744ae2
Kyungyoon Kim <ruddbs5302@gmail.com>
2022-10-24 23:39:36 +0900

    refactor: deprecated exhastive extension

--- src/main/kotlin/io/pemassi/kotlin/extensions/grammer/WhenExtension.kt
+++ src/main/kotlin/io/pemassi/kotlin/extensions/grammer/WhenExtension.kt
@@ -1,3 +1,8 @@
 package io.pemassi.kotlin.extensions.grammer
 
+@Deprecated(
+    message = "Recommend to use Kotlin above version 1.7.0",
+    level = DeprecationLevel.WARNING,
+    replaceWith = ReplaceWith("")
+)
 fun Any?.exhaustive() = Unit
